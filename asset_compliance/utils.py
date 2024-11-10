3d34ada1e3122627c2feab05566d635380ac63f0
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-11-10 23:09:35 +0500

    feat: function that overrides the core function of calculating periodicity

---
+++ asset_compliance/utils.py
@@ -0,0 +1,48 @@
+import frappe
+from frappe.utils import add_days, add_months, add_years, getdate, nowdate
+
+
+@frappe.whitelist()
+def custom_calculate_next_due_date(
+    periodicity,
+    start_date=None,
+    end_date=None,
+    last_completion_date=None,
+    next_due_date=None,
+):
+    if not start_date and not last_completion_date:
+        start_date = frappe.utils.now()
+
+    if last_completion_date and (
+        (start_date and last_completion_date > start_date) or not start_date
+    ):
+        start_date = last_completion_date
+    if periodicity == "Daily":
+        next_due_date = add_days(start_date, 1)
+    if periodicity == "Weekly":
+        next_due_date = add_days(start_date, 7)
+    if periodicity == "Monthly":
+        next_due_date = add_months(start_date, 1)
+    if periodicity == "Quarterly":
+        next_due_date = add_months(start_date, 3)
+    if periodicity == "Half-yearly":
+        next_due_date = add_months(start_date, 6)
+    if periodicity == "Yearly":
+        next_due_date = add_years(start_date, 1)
+    if periodicity == "2 Yearly":
+        next_due_date = add_years(start_date, 2)
+    if periodicity == "3 Yearly":
+        next_due_date = add_years(start_date, 3)
+    if periodicity == "4 Yearly":
+        next_due_date = add_years(start_date, 4)
+    if periodicity == "5 Yearly":
+        next_due_date = add_years(start_date, 5)
+    if periodicity == "6 Yearly":
+        next_due_date = add_years(start_date, 6)
+    if end_date and (
+        (start_date and start_date >= end_date)
+        or (last_completion_date and last_completion_date >= end_date)
+        or next_due_date
+    ):
+        next_due_date = ""
+    return next_due_date
